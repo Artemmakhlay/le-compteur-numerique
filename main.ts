aa299d3c29cb35282eb38ed49a85f8a9ba725a3a
Artemmakhlay <116097849+Artemmakhlay@users.noreply.github.com>
2022-10-24 15:17:48 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ let nombre_de_gens = 0
 input.onButtonPressed(Button.A, function () {
     nombre_de_gens += 0 + 1
     basic.showNumber(nombre_de_gens)
+    if (9 < nombre_de_gens) {
+        basic.showString("Avertissement")
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     control.reset()
